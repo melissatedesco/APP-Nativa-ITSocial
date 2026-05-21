9f17ce0e135841b4291ceaf022581105cfff6e11
Adry-72 <torchiaadriana@gmail.com>
2026-05-21 09:21:17 +0200

    vvvv
    vvvvv

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,6 @@
 //   • Emulatore Android   → http://10.0.2.2:8080
 //   • Simulatore iOS      → http://localhost:8080
 //
-export const HOST = 'http://192.168.1.100:8080';
+export const HOST = 'http://192.168.1.111:8080';
 // export const HOST = 'http://10.130.33.237:8080'
 export const API_BASE_URL = `${HOST}/api`
